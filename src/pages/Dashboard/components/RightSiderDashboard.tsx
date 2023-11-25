ae60d341e59d6ddaeba78d6f44a677ed8aaf3dfc
PhatVH <phatvh.work@gmail.com>
2023-11-25 20:04:59 +0700

    fix

--- src/pages/Dashboard/components/RightSiderDashboard.tsx
+++ src/pages/Dashboard/components/RightSiderDashboard.tsx
@@ -1,6 +1,5 @@
 import api from '@/api'
 import { RootState } from '@/store'
-import { Post } from '@/types'
 import { useRequest } from 'ahooks'
 import { Card } from 'antd'
 import { useState } from 'react'
@@ -41,7 +40,7 @@ const RightSiderDashboard = ({
 }: {
   onMajorChange?: (value?: number) => void
   onSubjectChange?: (value?: number) => void
-  onPostChange?: (value?: Post) => void
+  onPostChange?: (value?: string) => void
 }) => {
   const [majorSelect, setMajorSelect] = useState(-1)
   const [subjectSelect, setSubjectSelect] = useState(-1)
@@ -99,7 +98,7 @@ const RightSiderDashboard = ({
           onChange={(value) => {
             const post = trendingPost?.find((el) => el.id === value)
             setPostSelect(postSelect === value ? -1 : value)
-            onPostChange?.(postSelect === value ? undefined : post)
+            onPostChange?.(postSelect === value ? undefined : post?.title)
           }}
           title='Trending Post'
           idSelect={postSelect}
